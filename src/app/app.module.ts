375391a8989a9ae019fea5bd7b92609d79691430
AlexisNarvaez <alexiszed19@gmail.com>
2019-07-30 16:47:44 -0500

    Template formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TemplateComponent } from './components/template/template.component';
+import { DataComponent } from './components/data/data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateComponent,
+    DataComponent
   ],
   imports: [
     BrowserModule
