5a15b227c819d08010b7d1826b6d4df2291e0856
AlexisNarvaez <alexiszed19@gmail.com>
2019-07-31 22:03:13 -0500

    Formularios data validators required y pattern

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: []
 })
-export class DataComponent implements OnInit {
+export class DataComponent {
 
-  constructor() { }
+  forma: FormGroup;
 
-  ngOnInit() {
+  constructor() { 
+
+    this.forma = new FormGroup({
+      'nombre': new FormControl('', Validators.required ),
+      'apellido': new FormControl('', Validators.required ),
+      'correo': new FormControl('', Validators.required)
+    })
+
+
   }
+
+  guardarCambios(){
+    console.log(this.forma.value);
+    console.log(this.forma);
+  }
+
 
 }
