5a0c9de665da33405bed9962a10d42c93d3ef5bf
AlexisNarvaez <alexiszed19@gmail.com>
2019-08-01 16:34:34 -0500

    Agrupacion con objetos y reset and value data

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -10,20 +10,46 @@ export class DataComponent {
 
   forma: FormGroup;
 
+  usuario:any = {
+
+    nombreCompleto: {
+      nombre: "Alexis",
+      apellido: "Narvaez"
+    },
+    correo: "alexis97@hotmail.com"
+  }
+
   constructor() { 
 
+    console.log(this.usuario);
+
     this.forma = new FormGroup({
-      'nombre': new FormControl('', Validators.required ),
-      'apellido': new FormControl('', Validators.required ),
-      'correo': new FormControl('', Validators.required)
+      'nombreCompleto': new FormGroup({
+        'nombre': new FormControl( '' , [Validators.required, Validators.minLength(3)] ),
+        'apellido': new FormControl('' , Validators.required )
+      }),
+      'correo': new FormControl('' , [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
     })
 
+
+    this.forma.setValue(this.usuario);
 
   }
 
   guardarCambios(){
     console.log(this.forma.value);
     console.log(this.forma);
+
+
+    this.forma.reset({
+      nombreCompleto: {
+        nombre: "",
+        apellido: ""
+      },
+      correo: ""
+    });
+
+
   }
 
 
