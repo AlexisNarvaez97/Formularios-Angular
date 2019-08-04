4f33b8f32f6e591059fcd149eea4a6100ef5afaf
AlexisNarvaez <alexiszed19@gmail.com>
2019-08-04 17:27:53 -0500

    Formularios por data terminados, validaciones asincronas

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-data',
@@ -17,6 +18,7 @@ export class DataComponent {
       apellido: "Narvaez"
     },
     correo: "alexis97@hotmail.com"
+    // pasatiempos: ["Correr", "Dormir", "Comer"]
   }
 
   constructor() { 
@@ -26,13 +28,30 @@ export class DataComponent {
     this.forma = new FormGroup({
       'nombreCompleto': new FormGroup({
         'nombre': new FormControl( '' , [Validators.required, Validators.minLength(3)] ),
-        'apellido': new FormControl('' , Validators.required )
+        'apellido': new FormControl('' , [ this.noNarvaez, Validators.required] )
       }),
-      'correo': new FormControl('' , [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
+      'correo': new FormControl('' , [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]),
+      'pasatiempos': new FormArray([
+        new FormControl( 'Correr', Validators.required)
+      ]),
+      'username': new FormControl('', Validators.required, this.existeUsuario ),
+      'password1': new FormControl('', Validators.required),
+      'password2': new FormControl()
     })
 
+    //Llenar los input con el array de objetos.
+    // this.forma.setValue(this.usuario);
 
-    this.forma.setValue(this.usuario);
+
+    this.forma.controls['password2'].setValidators([ Validators.required, this.noIgual.bind(this.forma)]);
+
+    this.forma.controls['username'].valueChanges.subscribe (data => {
+      console.log(data);
+    })
+
+    this.forma.controls['username'].statusChanges.subscribe(data =>{
+      console.log(data);
+    })
 
   }
 
@@ -41,16 +60,67 @@ export class DataComponent {
     console.log(this.forma);
 
 
-    this.forma.reset({
-      nombreCompleto: {
-        nombre: "",
-        apellido: ""
-      },
-      correo: ""
-    });
+    // this.forma.reset({
+    //   nombreCompleto: {
+    //     nombre: "",
+    //     apellido: ""
+    //   },
+    //   correo: ""
+    // });
 
 
   }
+
+
+  //Agregar un pasatiempo nuevo dando push.
+  agregarPasatiempo(){
+
+    (<FormArray>this.forma.controls['pasatiempos']).push(new FormControl("Dormir", Validators.required));
+
+  }
+
+
+  //Validación personalizada.
+  noNarvaez(control: FormControl ): {[s:string]: boolean } {
+    
+
+    if(control.value === "narvaez"){
+      return {
+        nonarvaez:true
+      }
+    }
+    return null;
+  }
+
+
+  noIgual(control: FormControl ): {[s:string]: boolean } {
+
+    // console.log(this);
+    let forma:any = this;
+
+    if(control.value !== forma.controls['password1'].value ){
+      return {
+        noiguales:true
+      }
+    }
+    return null;
+  }
+
+  existeUsuario( control: FormControl ): Promise<any>|Observable<any>{
+
+    let promesa= new Promise(
+      (resolve, reject) =>{
+        setTimeout(()=>{
+          if(control.value === "aletsnarvaez"){
+            resolve ( {existe:true} )
+          } else{
+            resolve ( null )
+          }
+        }, 3000)
+      }
+    )
+    return promesa;
+  } 
 
 
 }
